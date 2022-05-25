fe1401b70584b2269696cba252a3ff205d52b21a
sofia <sofia.pinuelam@autonoma.edu.co>
2022-05-25 06:50:21 -0500

    Se conecto con el client

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const { API_VERSION } = require("./config");
@@ -8,6 +9,8 @@ const userRoutes = require("./src/routes/user.router")
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+//evitar bloqueo del cors
+app.use(cors());
 
 app.use(`/api/${API_VERSION}`, userRoutes);
 
